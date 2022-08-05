43407dae3af8cb3a8accaaf5d716e01936869c2b
Aa-thomas <Aartho95@gmail.com>
2022-08-05 15:45:13 -0400

    update link to 'flowers'

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (                  
     <nav className="main-nav">
         <ul>
-            <li><Link to="/sunsets">Sunsets</Link></li>
+            <li><Link to="/Flowers">Flowers</Link></li>
             <li><Link to="/waterfalls">Waterfalls</Link></li>
             <li><Link to="/rainbows">Rainbows</Link></li>
         </ul>
