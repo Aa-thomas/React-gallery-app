0ada492eff1c9ab4b581061fc3cced3447f9aacf
Aa-thomas <Aartho95@gmail.com>
2022-08-05 11:42:18 -0400

    convert to functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import {React, useEffect, useState} from 'react';
 import Error404 from './components/Error404';
 import SearchForm from './components/SearchForm';
 import Navigation from './components/Navigation';
@@ -8,47 +8,52 @@ import apiKey from './config';
 import axios from 'axios';
 import { BrowserRouter, Route, Routes, } from 'react-router-dom';
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    photos: [],
-    sunsets: [],
-    waterfalls: [],
-    rainbows: [],
-    loading: true,
-  };
+  // state = {
+  //   photos: [],
+  //   flowers: [],
+  //   waterfalls: [],
+  //   rainbows: [],
+  //   loading: true,
+  // };
+  const [photos, setPhotos] = useState([]);
+  const [flowers, setFlowers] = useState([]);
+  const [waterfalls, setWaterfalls] = useState([]);
+  const [rainbows, setRainbows] = useState([]);
+  const [loading, setLoading] = useState('true');
   
-  componentDidMount() {
-    this.performSearch();
-    this.performSearch('sunsets');
-    this.performSearch('rainbows');
-    this.performSearch('waterfalls');
-  }
+  useEffect( () => {
+    performSearch();
+    performSearch('flowers');
+    performSearch('rainbows');
+    performSearch('waterfalls');
+  }, [] )
   
-  performSearch = (query) => {
+  const performSearch = (query = 'ocean') => {
     const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
 
     axios.get(flickrUrl)
       .then(response => {
-        this.setState({
-          photos: response.data.photos.photo,
-          loading: false
-        });
-      if (query === 'sunsets') {
-        this.setState({
-          sunsets: response.data.photos.photo,
-          loading: false
-        })};
+        
+          setPhotos(response.data.photos.photo);
+          setLoading(false);
+        
+        if (query === 'flowers') {
+        
+          setFlowers(response.data.photos.photo);
+          setLoading(false);
+        };
       if (query === 'rainbows') {
-        this.setState({
-          rainbows: response.data.photos.photo,
-          loading: false
-        })};
+        
+          setRainbows(response.data.photos.photo);
+          setLoading(false);
+        };
       if (query === 'waterfalls') {
-        this.setState({
-          waterfalls: response.data.photos.photo,
-          loading: false
-        })};
+        
+          setWaterfalls(response.data.photos.photo);
+          setLoading(false);
+        };
         
       })
       .catch(error => {
@@ -57,25 +62,25 @@ class App extends React.Component {
     
   }
 
-  render() {
+  
     
-    console.log(this.state.photos)
+    console.log(photos)
     return (
       
       <div>
       <BrowserRouter>
         <h1>React Gallery</h1>
-        <SearchForm onSearch={this.performSearch} />
+        <SearchForm onSearch={performSearch} />
         <Navigation />        
         {
-          (this.state.loading) ? 
+          (loading) ? 
             <p>Loading...</p>
           : <Routes>                
-              <Route exact path="/" element={<PhotoList data={this.state.photos} />} />
-              <Route path="/sunsets" element={<PhotoList data={this.state.sunsets} />} />
-              <Route path="/waterfalls" element={<PhotoList data={this.state.waterfalls} /> }/>
-              <Route path="/rainbows" element={<PhotoList data={this.state.rainbows} />}/>
-              <Route path="/:query" element={<PhotoList data={this.state.photos}  />} />
+              <Route exact path="/" element={<PhotoList data={photos} />} />
+              <Route path="/flowers" element={<PhotoList data={flowers} />} />
+              <Route path="/waterfalls" element={<PhotoList data={waterfalls} /> }/>
+              <Route path="/rainbows" element={<PhotoList data={rainbows} />}/>
+              <Route path="/search/:query" element={<PhotoList data={photos}  />} />
               <Route path="*" element={<Error404 />} />
             </Routes> 
         }
@@ -84,7 +89,7 @@ class App extends React.Component {
       
     );
     
-  }
+  
 }
 
 export default App;
