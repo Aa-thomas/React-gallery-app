4e1214a846eed961d14681c982ac3ffc51a6c10b
Aa-thomas <aarthomas01@gmail.com>
2022-11-24 11:34:06 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,90 +1,107 @@
 import './App.css';
-import {React, useEffect, useState} from 'react';
+import { React, useEffect, useState } from 'react';
 import Error404 from './components/Error404';
 import SearchForm from './components/SearchForm';
 import Navigation from './components/Navigation';
 import PhotoList from './components/PhotoList';
 import apiKey from './config';
 import axios from 'axios';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
+	const [photos, setPhotos] = useState([]);
+	const [flowers, setFlowers] = useState([]);
+	const [waterfalls, setWaterfalls] = useState([]);
+	const [rainbows, setRainbows] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [query, setQuery] = useState('ocean');
 
-  const [photos, setPhotos] = useState([]);
-  const [flowers, setFlowers] = useState([]);
-  const [waterfalls, setWaterfalls] = useState([]);
-  const [rainbows, setRainbows] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [query, setQuery] = useState('ocean');
-  
-  
+	const performSearch = (query) => {
+		const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
 
+		axios
+			.get(flickrUrl)
+			.then((response) => {
+				setPhotos(response.data.photos.photo);
+				setQuery(query);
+				setLoading(false);
+				if (query === 'flowers') {
+					setFlowers(response.data.photos.photo);
+					setQuery(query);
+					setLoading(false);
+				}
+				if (query === 'rainbows') {
+					setRainbows(response.data.photos.photo);
+					setQuery(query);
+					setLoading(false);
+				}
+				if (query === 'waterfalls') {
+					setWaterfalls(response.data.photos.photo);
+					setQuery(query);
+					setLoading(false);
+				}
+			})
+			.catch((error) => {
+				console.log('Error fetching and parsing data', error);
+			});
+	};
 
-  
-  const performSearch = (query) => {
-    const flickrUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
+	useEffect(() => {
+		performSearch('flowers');
+		performSearch('rainbows');
+		performSearch('waterfalls');
+		performSearch(query);
+	}, []);
 
-    axios.get(flickrUrl)
-      .then(response => {
-          setPhotos(response.data.photos.photo);
-          setQuery(query);
-          setLoading(false);
-        if (query === 'flowers') {
-          setFlowers(response.data.photos.photo);
-          setQuery(query);
-          setLoading(false);
-        };
-        if (query === 'rainbows') {
-          setRainbows(response.data.photos.photo);
-          setQuery(query);
-          setLoading(false);
-        };
-        if (query === 'waterfalls') {
-          setWaterfalls(response.data.photos.photo);
-          setQuery(query);
-          setLoading(false);
-        };
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error)
-      });
-    
-  }
-
-  useEffect( () => {
-    performSearch('flowers');
-    performSearch('rainbows');
-    performSearch('waterfalls');
-    performSearch(query);
-  }, [] )
-  
-    
-    // console.log(photos)
-    return (
-      
-      <div>
-      <BrowserRouter>
-        <h1>React Gallery</h1>
-        <SearchForm onSearch={performSearch} />
-        <Navigation />        
-        {
-          (loading) ? 
-            <p>Loading...</p>
-          : <Routes>                
-              <Route exact path="/" element={<PhotoList data={photos} performSearch={performSearch} />} />
-              <Route path="/flowers" element={<PhotoList data={flowers} />} />
-              <Route path="/waterfalls" element={<PhotoList data={waterfalls} /> }/>
-              <Route path="/rainbows" element={<PhotoList data={rainbows} />}/>
-              <Route path="/:search" exact element={<PhotoList data={photos} performSearch={performSearch} />} />
-              <Route path="*" element={<Error404 />} />
-            </Routes> 
-        }
-        </BrowserRouter>
-      </div>
-      
-    );
-    
-  
-}
+	// console.log(photos)
+	return (
+		<div>
+			<BrowserRouter>
+				<h1>React Gallery</h1>
+				<SearchForm onSearch={performSearch} />
+				<Navigation />
+				{loading ? (
+					<p>Loading...</p>
+				) : (
+					<Routes>
+						<Route
+							exact
+							path="/"
+							element={
+								<PhotoList
+									data={photos}
+									performSearch={performSearch}
+								/>
+							}
+						/>
+						<Route
+							path="/flowers"
+							element={<PhotoList data={flowers} />}
+						/>
+						<Route
+							path="/waterfalls"
+							element={<PhotoList data={waterfalls} />}
+						/>
+						<Route
+							path="/rainbows"
+							element={<PhotoList data={rainbows} />}
+						/>
+						<Route
+							path="/:search"
+							exact
+							element={
+								<PhotoList
+									data={photos}
+									performSearch={performSearch}
+								/>
+							}
+						/>
+						<Route path="*" element={<Error404 />} />
+					</Routes>
+				)}
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -4,37 +4,35 @@ import Photo from './Photo';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
-const PhotoList = ({data, performSearch}) => {
-    
-    const results = data; 
-    let photos;
+const PhotoList = ({ data, performSearch }) => {
+	const results = data;
+	let photos;
 
-    const { search } = useParams();
+	const { search } = useParams();
 
-    useEffect(() => {
-        if (search) {
-            performSearch(search);
-        }
-    }, [search]);
-    
-    if (results.length > 0) {
-        photos = results.map( photo => 
-            <Photo 
-                url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
-                key={photo.id}
-            />
-    )} else {
-        photos = <NotFound />
-    }
-    
-    return(
-        <div className="photo-container">
-        <h2>Results</h2>
-            <ul>
-                {photos}
-            </ul>
-        </div>
-    )
-}
+	useEffect(() => {
+		if (search) {
+			performSearch(search);
+		}
+	}, [search]);
+
+	if (results.length > 0) {
+		photos = results.map((photo) => (
+			<Photo
+				url={`https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+				key={photo.id}
+			/>
+		));
+	} else {
+		photos = <NotFound />;
+	}
+
+	return (
+		<div className="photo-container">
+			<h2>Results</h2>
+			<ul>{photos}</ul>
+		</div>
+	);
+};
 
 export default PhotoList;
